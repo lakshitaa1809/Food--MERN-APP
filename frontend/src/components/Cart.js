9add499e123fdfa91216ce521ad1685d3e8796b8
Saranya <tsaranya03@gmail.com>
2023-06-18 00:33:30 +0530

    first commit

---
+++ frontend/src/components/Cart.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Cart.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AiOutlineClose } from "react-icons/ai";
+import { removeProduct } from "../redux/cartSlice";
+import { useNavigate } from "react-router-dom";
+
+const Cart = () => {
+  const { products } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  let totalPrice = 0;
+  products.map((product) => (totalPrice += product.quantity * product.price));
+
+  const handleRemoveProduct = (id) => {
+    console.log(id);
+    dispatch(removeProduct({ _id: id }));
+  };
+
+  const handleOrder = () => {
+    if (products.length > 0) {
+      navigate("/checkout");
+    }
+  };
+
+  return (
+    <div className="Cart_container">
+      <div className="Cart_wrapper">
+        <div className="Cart_left">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <div key={product._id} className="Cart_product">
+                <div
+                  onClick={() => handleRemoveProduct(product._id)}
+                  className="Cart_closeBtn"
+                >
+                  <AiOutlineClose />
+                </div>
+                <img
+                  src={`http://localhost:5010/images/${product.img}`}
+                  className="Card_img"
+                  alt="Card_img"
+                />
+                <div className="Cart_productData">
+                  <h3 className="Cart_title">{product.title}</h3>
+                  <div className="Cart_productAndQuantity">
+                    <span className="Cart_quantity">{product.quantity} </span>
+                    <span className="Cart_price">
+                      <span>INR </span>
+                      {product.price}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h1 className="Cart_noProducts">
+              No products in the cart. Go shopping!
+            </h1>
+          )}
+        </div>
+        <div className="Cart_right">
+          <div className="Cart_totalProductMsg">
+            Total products: {products.length}
+          </div>
+          <div className="Cart_subtotalCheckoutBtns">
+            <span className="Cart_subtotal">Total: INR {totalPrice}</span>
+            <span
+              onClick={handleOrder}
+              disabled={products.length === 0}
+              className="Card_orderNowBtn"
+            >
+              Order now
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/authSlice";
+
+const Navbar = () => {
+  const { products } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
+  return (
+    <div className="Nav_container">
+      <div className="Nav_wrapper">
+        <div className="Nav_left">
+          <img
+            className="Logo"
+            src="https://res.cloudinary.com/dehghhzey/image/upload/v1684640390/logo_glmbmg.jpg"
+            alt="logo"
+          />
+        </div>
+        <div className="Nav_center">
+          <ul className="Nav_list">
+            <li className="Nav_listItem">
+              <a href="#">Home</a>
+            </li>
+            <li className="Nav_listItem">
+              <a href="#items">Items</a>
+            </li>
+
+            <li className="Nav_listItem">
+              <a href="#contacts">Contacts</a>
+            </li>
+            <li className="Nav_listItem">
+              <Link to="/create">Create</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="Nav_right">
+          <AiOutlineUser className="UserIcon" />
+          <Link to="/cart" className="cart_container">
+            <AiOutlineShoppingCart className="CartIcon" />
+            <div className="Nav_CartQuantity">{products.length}</div>
+          </Link>
+          <button onClick={handleLogout} className="Logout">
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
